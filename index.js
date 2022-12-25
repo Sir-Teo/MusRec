05c339939572f4f88e33533da1214a58df84b16a
Teo <32597645+Sir-Teo@users.noreply.github.com>
2022-12-25 13:53:59 -0800

    add conversion source code

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+//Client ID deca4b76e5114fdbb97ce87467341145
+//Client Secret 93f8fc29f4a3427881a6228835eefd16
+
+
+
 function readURL(input) {
     if (input.files && input.files[0]) {
       var reader = new FileReader();
